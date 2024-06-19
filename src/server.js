75e23a66606a89db6e8f5f8cf545caa4f09427fb
OpamLNL <nataliya.linchuk@uzhnu.edu.ua>
2024-06-19 22:52:22 +0300

    client http fix 2

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const {closePool} = require("./config/database");
 
 // Middleware для обробки CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://movi-maze-epicsagas-projects.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'movi-maze-client.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
